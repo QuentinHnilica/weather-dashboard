893dc95e70a7ff25ac953e416414250e4bd29a62
unknown <Quentinhnilica@gmail.com>
2021-08-30 21:53:40 -0700

    icons added and change based on weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,19 @@ $(document).ready(function(){
     var startURL = 'https://api.openweathermap.org/data/2.5/onecall?'
     var apiId = '&appid=eba6d54703d207f3af8bc307df016cab'
     var cityUrl = 'https://api.openweathermap.org/data/2.5/weather?q='
+    var iconUrl = 'http://openweathermap.org/img/wn/'
     var location = 'San Diego'
     var date = new Date()
     var today = date.toDateString()
-    var week = date.getDate()
     var tomorrow
 
     function cardInfo(card, info, nextDay) {
         tomorrow = new Date(new Date().setDate(date.getDate() + nextDay))
 
         card.children()[0].innerText = tomorrow.toDateString()
-        //card.children()[1].innerText = 'bruh'
+        console.log(info.weather[0].icon)
+        card.children()[1].setAttribute('src', iconUrl + info.weather[0].icon + '.png')
+        card.children()[1].setAttribute('width', '50')
         card.children()[2].innerText = 'Temp: ' + info.temp.day
         card.children()[3].innerText = 'Wind: ' + info.wind_speed
         card.children()[4].innerText = 'Humidity: ' + info.humidity
@@ -25,6 +27,8 @@ $(document).ready(function(){
     function displayWeather(info){
         console.log(info)
         $('.area').text(location + ' : ' + today)
+        $('.area').append(document.createElement('img'))
+        $('.area').children().attr('src', iconUrl + info.current.weather[0].icon + '.png')
         $('#temp').text('Current Temp: ' + info.current.temp)
         $('#wind').text('Wind Speed: ' + info.current.wind_speed)
         $('#humid').text('Humidity: ' + info.current.humidity)
