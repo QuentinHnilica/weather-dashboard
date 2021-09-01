42bacf5aa508ac1c8ed53a5467b0dbdd5c1aef34
unknown <Quentinhnilica@gmail.com>
2021-09-01 15:26:46 -0700

    added comments to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,15 @@ $(document).ready(function(){
     var apiId = '&appid=eba6d54703d207f3af8bc307df016cab'
     var cityUrl = 'https://api.openweathermap.org/data/2.5/weather?q='
     var iconUrl = 'http://openweathermap.org/img/wn/'
-    var location = 'San Diego'
+    var location = 'San Diego' //So that default page is SanDiego
     var date = new Date()
     var today = date.toDateString()
     var tomorrow
 
-    function cardInfo(card, info, nextDay) {
+    function cardInfo(card, info, nextDay) { //displays all the weather properties
         tomorrow = new Date(new Date().setDate(date.getDate() + nextDay))
 
         card.children()[0].innerText = tomorrow.toDateString()
-        console.log(info.weather[0].icon)
         card.children()[1].setAttribute('src', iconUrl + info.weather[0].icon + '.png')
         card.children()[1].setAttribute('width', '50')
         card.children()[2].innerText = 'Temp: ' + info.temp.day
@@ -20,8 +19,7 @@ $(document).ready(function(){
         card.children()[4].innerText = 'Humidity: ' + info.humidity
     }
 
-    function displayWeather(info){
-        console.log(info)
+    function displayWeather(info){ // big box current weather data
         $('.area').text(location + ' : ' + today)
         $('.area').append(document.createElement('img'))
         $('.area').children().attr('src', iconUrl + info.current.weather[0].icon + '.png')
@@ -30,8 +28,7 @@ $(document).ready(function(){
         $('#humid').text('Humidity: ' + info.current.humidity)
         $('#uv').text('UV Index: ' + info.current.uvi)
 
-        console.log($('.forecastCards'))
-        $('.forecastCards').children().each(function(i) {
+        $('.forecastCards').children().each(function(i) { // 5 day forcast cards
             switch(i){
                 case 0:{
                     cardInfo($(this), info.daily[i], i+1)
@@ -59,31 +56,20 @@ $(document).ready(function(){
 
     function getFetch(){  
         if (location != ''){
-            console.log(location)
-            fetch(cityUrl + location + apiId).then(function(response){
-                console.log(response)
+            fetch(cityUrl + location + apiId).then(function(response){ //fetch to get lat and lon
                 if (response.status === 404){
-                    
                     $('#searchCity').val('')
-                    $('#searchCity').attr('placeholder', 'Input A Different City')
+                    $('#searchCity').attr('placeholder', 'Input A Different City') //In case they input a city you can't fetch
                 }
                 else{
                     response.json().then(function(data){
                         locationObj = data.coord
                         if (locationObj != null){
-                            fetch(startURL + 'lat=' + locationObj.lat + '&lon=' + locationObj.lon + apiId)
+                            fetch(startURL + 'lat=' + locationObj.lat + '&lon=' + locationObj.lon + apiId) //fetch for the weather data
                                 .then(function(response){
-                                    console.log(response)
-                                    if (response.status === 404){
-    
-                                        $('#searchCity').text('')
-                                        $('#searchCity').attr('placeholder', 'Input A Different City')
-                                    }
-                                    else{
-                                        response.json().then(function(data){
-                                            displayWeather(data)
-                                        })
-                                    }
+                                    response.json().then(function(data){
+                                        displayWeather(data)
+                                    })
                                 })
                         } 
                         else{
@@ -98,15 +84,15 @@ $(document).ready(function(){
         }   
     }
 
-    $('#submitForm').on('submit', function(event){
+    $('#submitForm').on('submit', function(event){ //Gets location from your input
         event.preventDefault();
         location = $('#searchCity').val()
         getFetch()
     })
 
-    $('.buttonBox .formButton').on('click', function(event){
+    $('.buttonBox .formButton').on('click', function(event){ //Side Buttons For auto Search
         location = $(this)[0].innerText
         getFetch()
     })
-    getFetch()
+    getFetch() //Sets default page to San Diego 
 })
